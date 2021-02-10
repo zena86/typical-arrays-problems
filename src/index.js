40d69cc1471f3e0db9c283f79726a1df06c61579
zena86 <76158579+zena86@users.noreply.github.com>
2021-02-10 19:27:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == null || array.length == 0) return 0;
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) min = array[i];
+  } 
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == null || array.length == 0) return 0;
+  let max = array[0]; 
+  for (let i = 0; i < array.length; i++) { 
+    if (max < array[i]) max = array[i]; 
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array == null || array.length == 0) return 0;
+  let sumOfArray = array.reduce(function(sum, elem){
+	  return sum + elem;
+  }, 0);
+  return sumOfArray/array.length;
 }
